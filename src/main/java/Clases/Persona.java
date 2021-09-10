e34e763b9e044cca7096662c3543726e85347fbf
Luis <lsaico4253@outlook.com>
2021-09-10 16:57:59 -0500

    Creación del proyecto

---
+++ src/main/java/Clases/Persona.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author lsaic
+ */
+public abstract class Persona {
+    private int id;
+    private int rol;
+    private String cedula;
+
+    public Persona(int id, int rol, String cedula) {
+        this.id = id;
+        this.rol = rol;
+        this.cedula = cedula;
+    }
+
+    public Persona() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+}
