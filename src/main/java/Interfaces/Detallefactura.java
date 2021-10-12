bf3fdcc00eb08338dea783e69377297fe85aabcc
CrisJavi1 <cristianillescas410@gmail.com>
2021-10-12 02:25:26 -0500

    Se agrego la interfaz detalle factura

--- src/main/java/Interfaces/Detallefactura.java
+++ src/main/java/Interfaces/Detallefactura.java
@@ -5,19 +5,95 @@
  */
 package Interfaces;
 
+import static Clases.ConexionBD.close;
+import static Clases.ConexionBD.getConnection;
+import java.sql.SQLException;
+import Clases.Tratamientoo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author HP
  */
 public class Detallefactura extends javax.swing.JFrame {
-
+  ArrayList<Tratamientoo> detalle= new ArrayList <>();
+    private static final String SQL_SELECT = "SELECT * FROM Tratamiento";
+    
     /**
      * Creates new form Detallefactura
      */
+    
     public Detallefactura() {
         initComponents();
+         this.setLocationRelativeTo(null);
+        detalle=seleccionar();
+        mostrar();
     }
 
+     public ArrayList <Tratamientoo> seleccionar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Tratamientoo tratamiento = null;
+        ArrayList <Tratamientoo> pacientes = new ArrayList<>();
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+//                int idTratamientoo = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                String tipo = rs.getString("tipo");
+                String duracionaño = rs.getString("duracionaño");
+                String duracionmes = rs.getString("duracionmes");
+                String duraciondia = rs.getString("duraciondia");
+                String costo = rs.getString("costo");
+              
+
+                tratamiento = new Tratamientoo(nombre, tipo, duracionaño, duracionmes, duraciondia, costo);
+
+                pacientes.add(tratamiento);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                close(rs);
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return pacientes;
+    }
+    
+    public void mostrar(){
+     String matrizp [][] = new String[detalle.size()][9];
+     
+     for (int i = 0; i < detalle.size(); i++) {
+        
+         matrizp[i][1] = detalle.get(i).getNombre();
+         matrizp[i][2] = detalle.get(i).getTipo();
+         matrizp[i][3] = detalle.get(i).getDuracionaño();
+         matrizp[i][4]=detalle.get(i).getDuracionmes();
+         matrizp[i][5] = detalle.get(i).getDuraciondia();
+         matrizp[i][6] = detalle.get(i).getCosto();
+        
+
+         }
+         TABLADETALLE.setModel(new javax.swing.table.DefaultTableModel(
+                matrizp,
+                new String[]{
+                    "Cédula", "Nombres", "Apellidos", "Edad","Genero","Num ficha","Afiliacion","Telefono","Telefono1"
+                }
+                 
+        ));
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,21 +103,115 @@ public class Detallefactura extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        TABLABUSQUEDA = new javax.swing.JTable();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        TABLADETALLE = new javax.swing.JTable();
+        BUSCADOR = new javax.swing.JTextField();
+        BUSCAR = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("DETALLE DE LA FACTURA");
+
+        TABLABUSQUEDA.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "ID TRATAMIENTO", "NOMBRE", "TIPO", "DURACION AÑO", "DURACION MES", "DURACION DIA", "COSTO"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(TABLABUSQUEDA);
+
+        TABLADETALLE.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "ID TRATAMIENTO", "NOMBRE", "TIPO", "DURACION AÑO", "DURACION MES", "DURACION DIA", "COSTO"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane3.setViewportView(TABLADETALLE);
+
+        BUSCADOR.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BUSCADORActionPerformed(evt);
+            }
+        });
+
+        BUSCAR.setText("BUSCAR");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jScrollPane2)
+            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 792, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(282, 282, 282)
+                .addComponent(BUSCAR)
+                .addGap(29, 29, 29)
+                .addComponent(BUSCADOR, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(326, 326, 326))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(BUSCADOR, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BUSCAR))
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BUSCADORActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BUSCADORActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BUSCADORActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +249,12 @@ public class Detallefactura extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField BUSCADOR;
+    private javax.swing.JLabel BUSCAR;
+    private javax.swing.JTable TABLABUSQUEDA;
+    private javax.swing.JTable TABLADETALLE;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     // End of variables declaration//GEN-END:variables
 }
