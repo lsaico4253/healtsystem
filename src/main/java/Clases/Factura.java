1ffe790da31bbc8b6b4dd8be98c55ca7a7518a87
CrisJavi1 <cristianillescas410@gmail.com>
2021-10-12 07:59:17 -0500

    Se agregaron los datos de la clase factura

--- src/main/java/Clases/Factura.java
+++ src/main/java/Clases/Factura.java
@@ -10,6 +10,43 @@ package Clases;
  * @author lsaic
  */
 public class Factura {
+    private int numero_factura;
+    private double costofinal;
+    private String fecha;
+
+    public Factura() {
+    }
+
+    public Factura(int numero_factura, double costofinal, String fecha) {
+        this.numero_factura = numero_factura;
+        this.costofinal = costofinal;
+        this.fecha = fecha;
+    }
+
+    public int getNumero_factura() {
+        return numero_factura;
+    }
+
+    public void setNumero_factura(int numero_factura) {
+        this.numero_factura = numero_factura;
+    }
+
+    public double getCostofinal() {
+        return costofinal;
+    }
+
+    public void setCostofinal(double costofinal) {
+        this.costofinal = costofinal;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
     
     
 }
